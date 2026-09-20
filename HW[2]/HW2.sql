0cebc0d1fd33f6007092e3b82f080b20f3dccb42
Jaire Bartlett <jairebartlett06@gmail.com>
2026-09-20 18:28:11 -0400

    Add files via upload

---
+++ HW[2]/HW2.sql
@@ -0,0 +1,180 @@
+-- *************************************************************
+-- HW2.sql: Database Schema Creation and Constraints
+-- *************************************************************
+
+-- *************************************************************
+-- Safe Updates
+-- *************************************************************
+set SQL_SAFE_UPDATES = 0;
+set FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS employs;
+DROP TABLE IF EXISTS offers;
+DROP TABLE IF EXISTS baristas;
+DROP TABLE IF EXISTS shops;
+DROP TABLE IF EXISTS pastries;
+
+-- *************************************************************
+-- 1. Create Tables for Entity Sets
+-- *************************************************************
+
+-- Table: pastries
+create table pastries (
+    pastryID    integer not null,
+    name        varchar (50) not null,
+    category    varchar (30),
+    price       decimal (5,2),
+    primary key (pastryID)
+);
+
+-- Table: baristas
+create table baristas (
+    baristaID        integer not null,
+    name             varchar (50) not null,
+    experience_level varchar (20),
+    primary key (baristaID)
+);
+
+-- Table: shops
+create table shops (
+    shopID   integer not null,
+    name     varchar (50),
+    city     varchar (30),
+    primary key (shopID)
+);
+
+-- *************************************************************
+-- 2. Create Relationship Tables (Contain Foreign Keys)
+-- *************************************************************
+
+-- Table: offers
+-- Uses a Composite Primary Key because it is a relationship 
+-- by the combination of a specific shop offering a specific pastry.
+CREATE TABLE offers (
+    shopID     integer not null,
+    pastryID   integer not null,
+    date_added date,
+    primary key (shopID, pastryID), -- Composite Primary Key
+    foreign key (shopID) references shops(shopID),
+    foreign key (pastryID) references pastries(pastryID)
+);
+
+-- Table: employs
+-- Uses a Composite Primary Key because it is a relationship
+-- by a specific barista working at a specific shop.
+create table employs (
+    baristaID integer not null,
+    shopID    integer not null,
+    primary key (baristaID, shopID), -- Composite Primary Key
+    foreign key (baristaID) references baristas(baristaID),
+    foreign key (shopID) references shops(shopID)
+);
+
+-- *************************************************************
+-- 3. Queries
+-- *************************************************************
+
+use hw1;
+ 
+-- 1.) Find the average price of pastries for each category from the pastries table
+
+select category, AVG(price) as 'AvgPrice'
+from pastries
+group by category;
+
+
+
+
+-- 2.) Find the total number of baristas at each experience level from the baristas table
+
+select * from baristas;
+
+select experience_level, count(experience_level) as 'NumOfBaristas'
+from baristas
+group by experience_level;
+
+-- 3.) Count the total number of shops located in each city from the shops table
+
+select * from shops;
+
+select city, count(shopID) as 'NumOfShops'
+from shops
+group by city;
+
+
+-- 4.) Find the maximum price among pastries for each category from the pastries table.
+
+select * from pastries;
+
+select category, max(price) as 'MaxPrice'
+from pastries
+group by category;
+
+-- 5.) Count how many pastries have been added by each shop using the shopID column from 
+-- the offers table.
+
+select * from offers;
+
+select shopID, count(pastryID) as 'NumOfPastries'
+from offers
+group by shopID;
+
+
+-- 6.) Find the name, category, and price of any pastry whose price matches the maximum price within
+-- its category.
+
+select name, category, price
+from pastries
+where (category, price) in (
+    select category, MAX(price)
+    from pastries
+    group by category
+);
+
+-- 7.) Find the unique shop IDs from the offers table that have offered at 
+ -- least one pastry whose price is strictly greater than the overall avaerge price of all pastries
+ 
+select shopID
+ from offers o, pastries p
+ where o.pastryID = p.pastryID
+	and p.price > (select avg(price) from pastries)
+group by shopID;
+
+	
+
+ 
+ -- 8.) Find the shop ID and pastry ID for the record in the offers table that have the earliest
+ -- date_added (minimum date).
+ 
+ select shopID, pastryID
+ from offers
+ where date_added = (select min(date_added) from offers);
+ 
+ 
+ -- 9.) Find the shop ID(s) that offer the highest number of pastries, utilizing a subquery to evaluate
+ -- the maximum count per shop.
+ 
+select shopID, count(pastryID) as 'NumOfPastries'
+from offers o
+group by shopID
+having count(pastryID) >= all (
+		select count(pastryID)
+		from offers o
+        group by shopID
+);
+ 
+ -- 10.) Find the names of baristas who work at shops in 'Seattle' using nested subqueries.
+
+select * from employs;
+
+select name
+from baristas
+where baristaID in (
+	select baristaID
+    from employs
+    where shopID in (
+		select shopID
+        from shops
+        where city = 'Seattle'
+	)
+);
